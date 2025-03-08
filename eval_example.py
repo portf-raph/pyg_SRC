b69048ee9a986264c41d0b1ef2e2af65e79a7d5c
portf-raph <voraphy@gmail.com>
2025-03-08 17:31:28 +0000

    run and eval scripts added/modified

---
+++ eval_example.py
@@ -0,0 +1,89 @@
+import os
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+from torch.utils.data import DataLoader
+
+
+from model.SRCNet import SRCNet
+from data.pth_dataset import PthDataset
+from utils.train_helper import load_model, get_config
+
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+
+# 1. Load data
+dataset = PthDataset(load_dir='./data/PROTEINS/pth/train')
+data_loader = DataLoader(dataset, batch_size=8, shuffle=False, collate_fn=lambda x: x)
+
+# 2. Load model
+GIN_cfg = get_config('config/DEFAULT/DEF_GIN_cfg.json')
+SC_cfg = get_config('config/DEFAULT/DEF_SC_cfg.json')
+MLP_cfg = get_config('config/DEFAULT/DEF_MLP_cfg.json')
+model_class = "MLP"
+
+model = SRCNet(GIN_cfg=GIN_cfg,
+               SC_cfg=SC_cfg,
+               OUT_cfg=MLP_cfg,
+               model_class=model_class,
+               device=device)
+load_model(model=model, file_name='../exp/pyg_SRC/model_snapshot_best.pth', optimizer=None)
+model.train()
+
+# 3. Test train forward
+iter = 0
+for data_dicts in data_loader:
+
+    data_dicts = [{k: v.to(device) if isinstance(v, torch.Tensor)
+        else v for k, v in d.items()} for d in data_dicts]
+    out, A_fidelity, A_incoherence = model(data_dicts)
+    out = torch.softmax(out, dim=1)
+    print(out, A_fidelity, A_incoherence)
+    iter += 1
+    if iter == 10:
+        break
+
+# 4. Load graphs
+save_dir = '../exp/pyg_SRC'
+with open(os.path.join(save_dir, 'train_stats.p'), 'rb') as handle:
+    results = pickle.load(handle)
+plt.figure()
+plt.plot(results['train_step'], results['train_loss'], '-b', label='Perte')
+plt.xlabel("Nombre d'itérations")
+plt.ylabel("Perte d'entropie croisée")
+plt.title('Perte de classification')
+plt.savefig(os.path.join(save_dir, "Perte de classification"+".png"))
+plt.show()
+plt.close()
+
+plt.figure()
+plt.plot(results['train_A_fidelity'], '-b', label='Fidelite')
+plt.xlabel("Nombre d'époques")
+plt.ylabel("Erreur de fidélité")
+plt.title("Fidélité")
+plt.savefig(os.path.join(save_dir, "Fidélité"+".png"))
+plt.show()
+plt.close()
+
+plt.figure()
+plt.plot(results['train_A_incoherence'], '-r', label='Incohérence')
+plt.xlabel("Nombre d'époques")
+plt.ylabel("Erreur d'incohérence")
+plt.title("Incohérence")
+plt.savefig(os.path.join(save_dir, "Incohérence"+".png"))
+plt.show()
+plt.close()
+
+plt.figure()
+plt.plot(5*np.arange(1, len(results['val_epoch_acc'])+1), results['val_epoch_acc'], '-g', label="Exactitude de validation d'epoche")
+plt.xlabel("Nombre d'epoques")
+plt.ylabel("% de succès en classification")
+plt.title("Validation")
+plt.savefig(os.path.join(save_dir, "Validation"+".png"))
+plt.show()
+plt.close()
+
+
---
+++ run_example.py
@@ -0,0 +1,85 @@
+import os
+import sys
+import torch
+import pprint
+import logging
+import datetime
+import argparse
+import traceback
+
+from model.SRCNet import SRCNet
+from runner.pyg_runner import PYGRunner
+from data.pth_dataset import PthDataset
+from utils.train_helper import get_config
+
+
+def main():
+    #1. Parser
+    parser = argparse.ArgumentParser(
+          description="Running graph classification experiment"
+    )
+    parser.add_argument('--script_cfg', type=str, default='./config/DEFAULT/DEF_config.json')
+    parser.add_argument('--GIN_cfg', type=str, default='./config/DEFAULT/DEF_GIN_cfg.json')
+    parser.add_argument('--SC_cfg', type=str, default='./config/DEFAULT/DEF_SC_cfg.json')
+    parser.add_argument('--OUT_cfg', type=str, default='./config/DEFAULT/DEF_MLP_cfg.json')
+    parser.add_argument('--model_class', type=str, default='MLP')
+    parser.add_argument('--dataset_load_dir', type=str, default='./data/PROTEINS/pth/')
+    parser.add_argument('--log_level', type=str, default='INFO',
+                        help="Logging Level, \
+                          DEBUG, \
+                          INFO, \
+                          WARNING, \
+                          ERROR, \
+                          CRITICAL")
+    parser.add_argument('--comment', type=str, help="Experiment comment")
+    parser.add_argument('--test', type=str, default='False')
+    args = parser.parse_args()
+
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+
+    #2. Load model
+    script_cfg = get_config(args.script_cfg)
+    GIN_cfg = get_config(args.GIN_cfg)
+    SC_cfg = get_config(args.SC_cfg)
+    OUT_cfg = get_config(args.OUT_cfg)
+
+    model = SRCNet(
+        GIN_cfg=GIN_cfg,
+        SC_cfg=SC_cfg,
+        OUT_cfg=OUT_cfg,
+        model_class=args.model_class,
+        device=device
+    )
+    if not os.path.exists(script_cfg["save_dir"]):
+        os.makedirs(script_cfg["save_dir"])
+
+    #3. Dataset
+    train_dataset = PthDataset(load_dir=os.path.join(args.dataset_load_dir, 'train'))
+    dev_dataset = PthDataset(load_dir=os.path.join(args.dataset_load_dir, 'val'))
+
+    # 4. logger
+    log_file = '../exp/pyg_SRC/log_{}.txt'.format(datetime.datetime.now())
+    logging.basicConfig(level=args.log_level,
+                        filename=log_file,
+                        filemode='a',)
+    logger = logging.getLogger(__name__)
+    logger.info("Writing log file to {}".format(log_file))
+    logger.info("Exp instance id = {}".format(script_cfg["run_id"]))
+    logger.info("Exp comment = {}".format(args.comment))
+    logger.info("Config =")
+    print(">" * 80)
+    pprint.pprint(script_cfg)
+    print("<" * 80)
+
+    # 5. Runner
+    script_cfg["use_gpu"] = script_cfg["use_gpu"] and torch.cuda.is_available()
+    
+    runner = PYGRunner(model_object=model, script_cfg=script_cfg, logger=logger,
+                       train_dataset=train_dataset, dev_dataset=dev_dataset)
+    runner.train()    
+
+
+if __name__ == '__main__':
+    main()
